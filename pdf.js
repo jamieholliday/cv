83d1200ded9ac44d29042382516f53e970606f15
jamieholliday <jamie.holliday@myjobmatcher.com>
2016-03-29 20:27:59 +0100

    Generate pdf

---
+++ pdf.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var pdf = require('html-pdf');
+var html = fs.readFileSync('./dist/index.html', 'utf8');
+var options = {
+  format: 'Letter'
+};
+
+pdf.create(html, options).toFile('./dist/jamieholliday.pdf', function(err, res) {
+  if (err) return console.log(err);
+  console.log(res);
+});
